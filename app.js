cab95c9472fd0abda9e851725446334efbc9252e
goobway <calistagreenway@gmail.com>
2020-04-17 22:28:04 -0400

    creating start menu

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-window.addEventListener("keydown", moveAmoeba, false);
+// window.addEventListener("keydown", moveAmoeba, false);
 
-function moveAmoeba(e) {
-    switch (e.keyCode) {
-        case 37:
-            // left key pressed
-            deltaX -= 2;
-            break;
-        case 38:
-            // up key pressed
-            deltaY -= 2;
-            break;
-        case 39:
-            // right key pressed
-            deltaX += 2;
-            break;
-        case 40:
-            // down key pressed
-            deltaY += 2;
-            break;
-    }
-    // prevent browser from using default events
-    e.preventDefault();
-    // redraw amoeba after a key is pressed
-    drawModule.amoeba();
-}
+// function moveAmoeba(e) {
+//     switch (e.keyCode) {
+//         case 37:
+//             // left key pressed
+//             deltaX -= 2;
+//             break;
+//         case 38:
+//             // up key pressed
+//             deltaY -= 2;
+//             break;
+//         case 39:
+//             // right key pressed
+//             deltaX += 2;
+//             break;
+//         case 40:
+//             // down key pressed
+//             deltaY += 2;
+//             break;
+//     }
+//     // prevent browser from using default events
+//     e.preventDefault();
+//     // redraw amoeba after a key is pressed
+//     drawModule.amoeba();
+// }
--- draw.js
+++ draw.js
@@ -12,44 +12,44 @@ function startGame() {
 }
 
 
-var drawModule = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var food = function(num, radius, maxX, maxY) {
-        for (var i = 0; i <= num; i++) {
-            ctx.beginPath();
-            var randomX = Math.random(i) * maxX;
-            var randomY = Math.random(i) * maxY;
-            ctx.arc(randomX, randomY, radius, 0, 2 * Math.PI);
-            ctx.fillStyle = "blue";
-            ctx.fill();
-            ctx.closePath();
-        }
-    }
-    food(20, 5, 720, 480);
+// var drawModule = function() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     var food = function(num, radius, maxX, maxY) {
+//         for (var i = 0; i <= num; i++) {
+//             ctx.beginPath();
+//             var randomX = Math.random(i) * maxX;
+//             var randomY = Math.random(i) * maxY;
+//             ctx.arc(randomX, randomY, radius, 0, 2 * Math.PI);
+//             ctx.fillStyle = "blue";
+//             ctx.fill();
+//             ctx.closePath();
+//         }
+//     }
+//     food(20, 5, 720, 480);
 
-    var scoreBoard = function() {
-        // player score banner
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, 720, 48);
-        // player score text
-        ctx.fillStyle = "white";
-        ctx.font = "16pt sans-serif";
-        ctx.fillText("Score: " + score, 16, 32);
-    }
-    scoreBoard();
+//     var scoreBoard = function() {
+//         // player score banner
+//         ctx.fillStyle = "black";
+//         ctx.fillRect(0, 0, 720, 48);
+//         // player score text
+//         ctx.fillStyle = "white";
+//         ctx.font = "16pt sans-serif";
+//         ctx.fillText("Score: " + score, 16, 32);
+//     }
+//     scoreBoard();
 
-    var amoeba = function() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // amoeba
-        ctx.beginPath();
-        ctx.arc(centerX + deltaX, centerY + deltaY, 16, 0, 2 * Math.PI);
-        ctx.fillStyle = "green";
-        ctx.fill();
-        ctx.strokeStyle = "green";
-        ctx.stroke();
-        ctx.closePath();
-    }
-    drawModule.amoeba = amoeba;
-}
+//     var amoeba = function() {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//         // amoeba
+//         ctx.beginPath();
+//         ctx.arc(centerX + deltaX, centerY + deltaY, 16, 0, 2 * Math.PI);
+//         ctx.fillStyle = "green";
+//         ctx.fill();
+//         ctx.strokeStyle = "green";
+//         ctx.stroke();
+//         ctx.closePath();
+//     }
+//     drawModule.amoeba = amoeba;
+// }
 
-drawModule();
+// drawModule();
